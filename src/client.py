2de8a1ebc738e52e0ac84a0a863423106bdbf827
charlescohanlon <charlescohanlon@gmail.com>
2024-04-02 23:04:00 -0700

    fill it in

--- src/client.py
+++ src/client.py
@@ -12,71 +12,72 @@ TIME_FORMAT = r"%H:%M:%S"
 WAIT_TIME = 600
 
 # earlier than this => use ECMWF
-HRRR_CUTOFF_DATE = datetime.strptime("2014-07-30", DATE_FORMAT)
+MS_HRRR_CUTOFF = datetime.strptime("2021-03-21", DATE_FORMAT)
+AWSGOOGLE_HRRR_CUTOFF = datetime.strptime("2014-07-30", DATE_FORMAT)
 
 HRRR_SRC = "hrrr"
 ECMWF_SRC = "ifs"
 
 
-def worker(supplier, index):
+def parse_herbie(H: Herbie):
+    # TODO: fill this in with the parsing logic
+    return {}
+
+
+def client():
     while True:
         try:
             row_json = requests.get(SERVER_URL).json()
         except:
-            print(f"Server refused connection. Terminating worker {index}...")
+            print(f"Server refused connection. Terminating...")
             return
 
         idx = row_json["index"]
 
-        # needs to be before source
         date = datetime.strptime(row_json["date"], DATE_FORMAT)
-
-        source = HRRR_SRC if date >= HRRR_CUTOFF_DATE else ECMWF_SRC
-
         time = datetime.strptime(row_json["time"], TIME_FORMAT)
-
         combined = datetime.combine(date.date(), time.time())
 
-        print("supplier:", supplier, "datetime:", combined, "source:", source)
-        # H = Herbie(
-        #     str(combined),
-        #     model=source,
-        #     product="sfc",
-        #     fxx=[0],
-        # )
+        aws_flag = False
 
-        # parse herbie response
-        # convert to weather API params
+        def set_provider():
+            nonlocal aws_flag
+            aws_flag = not aws_flag
+            return "aws" if aws_flag else "google"
 
-        send_resp = requests.post(
-            SERVER_URL,
-            json={
-                "index": idx,
-                "main_temp": 2134,
-                "main_feels_like": 123,
-                "main_pressure": 23,
-                "main_humidity": 234,
-                "main_dew_point": 25,
-                "wind_speed": 903,
-                "wind_deg": 3490,
-                "wind_gust": 324,
-                "clouds_all": 0,
-                "rain_3h": 235,
-                "snow_3h": 345,
-                "visibility": 0,
-            },
-        )
-        if send_resp.status_code != 200:
-            print("Rate limit reached. Sleeping...")
-            sleep(WAIT_TIME)
-            print("Starting again.")
+        while True:
+            if date > MS_HRRR_CUTOFF:
+                model = HRRR_SRC
+                priority = "azure"
+            elif date > AWSGOOGLE_HRRR_CUTOFF:
+                model = HRRR_SRC
+                priority = set_provider()
+            else:
+                model = ECMWF_SRC
+                priority = set_provider()
 
+            H = Herbie(
+                str(combined),
+                model=model,
+                priority=priority,
+                product="sfc",
+                fxx=0,
+            )
+            if True:  # on success
+                break
 
-def main():
-    suppliers = ["aws"]
-    for i, supplier in enumerate(suppliers):
-        mp.Process(target=worker, args=(supplier, i)).start()
+        while True:
+            send_resp = requests.post(
+                SERVER_URL,
+                json=parse_herbie(H),
+            )
+            if send_resp.status_code == 200:
+                break
+            else:
+                print("Server Error:", send_resp.content.decode())
+                print("Sleeping...")
+                sleep(random.randrange(10, 100))
 
 
 if __name__ == "__main__":
-    main()
+    client()
